082752b624ed705f2235548551870fa0c59358c4
Matthew Sartain <matthewsartain@me.com>
2019-11-20 14:00:05 -0600

    render ticket price and seat

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,5 @@
 let Flight = require('../models/flight');
+let Ticket = require('../models/ticket');
 
 module.exports = {
     index,
@@ -30,7 +31,9 @@ function newFlight(req, res) {
 }
 
 function show(req, res){
-    Flight.findById(req.params.id, function(err, flight) {
-    res.render('flights/show', { title:'Flight Details', flight})
-});
-}
+    Flight.findById(req.params.id, function(err, flight){
+        Ticket.find({flight: flight._id}, function(err,tickets){
+            res.render('flights/show', { title:'Flight Details', flight, tickets})
+        });
+    })
+}    
